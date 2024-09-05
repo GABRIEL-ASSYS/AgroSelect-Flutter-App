56f43cbcaabf576264e47cc88803859daef77730
Gabriel Assys <engs-gabrieloliveira@camporeal.edu.br>
2024-09-05 17:09:32 -0300

    criando tela de favoritos

---
+++ lib/view/favoritos.dart
@@ -0,0 +1,13 @@
+class FavoritosScreen extends StatefulWidget {
+  const FavoritosScreen({super.key});
+
+  @override
+  State<FavoritosScreen> createState() => _FavoritosScreenState();
+}
+
+class _FavoritosScreenState extends State<FavoritosScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
