b2f88f7ca3bdb57adade40f2cf8681f3cd5802f7
Gabriel Assys <engs-gabrieloliveira@camporeal.edu.br>
2024-09-09 14:00:28 -0300

    Adicionar alert para confirmar saida da tela em editar relatorio

--- lib/view/relatorios/editar_relatorio.dart
+++ lib/view/relatorios/editar_relatorio.dart
@@ -138,12 +138,7 @@ class _EditarRelatorioScreenState extends State<EditarRelatorioScreen> {
         backgroundColor: Colors.green,
         leading: IconButton(
           onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const RelatoriosScreen(),
-              ),
-            );
+            _showExitConfirmationDialog(context);
           },
           icon: const Padding(
             padding: EdgeInsets.all(8.0),
@@ -239,6 +234,69 @@ class _EditarRelatorioScreenState extends State<EditarRelatorioScreen> {
           ),
         ),
       ),
+    );
+  }
+
+  void _showExitConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            'Sair',
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
+          ),
+          content: const Text(
+            'Nenhuma das alterações feitas será salva, você tem certeza que deseja sair?',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.green,
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                minimumSize: const Size(100, 50),
+              ),
+              child: const Text(
+                'Não',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                minimumSize: const Size(100, 50),
+              ),
+              child: const Text(
+                'Sim',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RelatoriosScreen()),
+                      (Route<dynamic> route) => false,
+                );
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
